7f952d4dcdf1585ce74e0123b3010adf0f4f0b15
Alexandr <downloadsite.x@gmail.com>
2019-11-28 00:50:55 +0200

    initial commit

---
+++ src/CRMServiceProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AVXMAN\CRM;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class CRMServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @param Filesystem $filesystem
+     * @return void
+     */
+    public function boot(Filesystem $filesystem){
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_route_table.php.stub' => $this->getMigrationFileName($filesystem),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__.'/../config/crm.php' => $this->getConfigFileName($filesystem),
+        ], 'config');
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            $this->app->configPath().DIRECTORY_SEPARATOR.'crm.php', 'crm'
+        );
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_route_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_route_table.php")
+            ->first();
+    }
+
+    /**
+     * Returns existing config file if found, else uses the current.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getConfigFileName(Filesystem $filesystem): string
+    {
+        return Collection::make($this->app->configPath().DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'crm.php');
+            })->push($this->app->configPath()."/crm.php")
+            ->first();
+    }
+
+}
---
+++ src/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AVXMAN\CRM;
+
+class Test{
+    static public function test(String $test) : String{
+        return 'Hi! I\'m X-MAN. And are you '.$test .'?';
+    }
+}
